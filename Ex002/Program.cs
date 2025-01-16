31621e0a87cb1a62397d5e794ebc069e59fe3e70
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-16 19:27:29 -0300

    Adicionar arquivos de projeto.

---
+++ Ex002/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 2;
+            float num = 2.35f;
+            char letra = 'c';
+            string nome = "Vinicius";
+
+            Console.WriteLine("O número é {0}.",numero);
+            Console.WriteLine("O valor é R$ {0}.",num);
+            Console.WriteLine("A letra é {0}.",letra);
+            Console.WriteLine("O nome é {0}.",nome);
+
+
+            Console.WriteLine("O nùmero é {0}, o valor é R${1}, a letra é {2} e o seu nome é {3}.",numero, num, letra, nome);
+        }
+    }
+}
---
+++ Ex005/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ex005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+
+            Console.WriteLine("Digite a primeira nota");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a proxima nota");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A sua média é:");
+
+            num3 = (num1 + num2) / 2;
+
+            Console.WriteLine("{0} + {1} / 2 = {2}", num1, num2, (num3 - num1) / 2);
+
+         
+
+
+
+
+
+
+
+        }
+    }
+}
