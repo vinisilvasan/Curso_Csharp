34fca72cd83bd39acd2291145325ddfa49d97027
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-16 21:06:48 -0300

    Atualização do exercício

---
+++ Ex007/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ex007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final = 50;
+            string resultado;
+
+            if (nota_final >= 60)
+            {
+                resultado = "Aprovado";
+
+            }
+            else
+            { 
+                resultado = "Reprovado";
+            }
+
+            Console.WriteLine("O aluno foi {0}.", resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
