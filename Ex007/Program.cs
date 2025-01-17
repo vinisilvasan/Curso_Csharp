ea99495ee73bff2db9f09b4b59fb70badc6c2450
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-16 22:10:14 -0300

    Alterações no sistema

--- Ex007/Program.cs
+++ Ex007/Program.cs
@@ -4,27 +4,49 @@
     {
         static void Main(string[] args)
         {
-            double nota_final = 30;
+            double nota_final, n1, n2, n3, n4;
             string resultado;
+
+            Console.Write("Digite a primeira nota do aluno: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota do aluno: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a terceira nota do aluno: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quarta nota do aluno: ");
+            n4 = Convert.ToDouble(Console.ReadLine());
+
+            nota_final = (n1 + n2 + n3 + n4) / 4;
 
             if (nota_final >= 60)
             {
-                resultado = "Aprovado";
-
+                if (nota_final >= 90)
+                {
+                    resultado = "Aprovado com louvor";
+                }
+                else
+                {
+                    resultado = "Aprovado";
+                }
             }
-
-
-            else if (nota_final >= 40)
-            {
-                resultado = "Recuperação"; 
-            }
-
             else
-            { 
-                resultado = "Reprovado";
+            {
+                if (nota_final >= 40)
+                {
+                    resultado = "Recuperação";
+                } 
+                else
+                {
+
+                    resultado = "Reprovado";
+                }
+               
             }
 
-            Console.WriteLine("O aluno foi {0}.", resultado);
+            Console.WriteLine("Nota do aluno {0} - Resultado: {1}.", nota_final, resultado);
 
 
 
