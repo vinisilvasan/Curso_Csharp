63cd197e78a73619fe5666cf1104ee701ad80c34
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-21 19:32:38 -0300

    exercicio de teste de triângulo

---
+++ Ex008/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("o número é par.");
+
+            }
+            else
+            {
+                Console.WriteLine("o número é impar.");
+
+            }
+
+
+
+
+
+
+        }
+
+    }
+}
+            
---
+++ Ex009/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime;
+
+namespace Ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoC;
+
+            Console.WriteLine("Programa para testar Triângulos: ");
+
+            Console.Write("Digite o valor do lado A: ");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado B: ");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado C: ");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA +ladoC) && (ladoC < ladoA + ladoB))
+            {
+                Console.WriteLine("Os valoes informados formam um triângulo.");
+
+            }
+            else
+            {
+                Console.WriteLine("Os valores informados não formam um triângulo.");
+
+
+            }
+
+        }
+    }
+}
