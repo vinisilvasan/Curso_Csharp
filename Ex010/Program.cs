7869d17cbb76282feccb194d30a9453e7d9e8f64
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-21 22:25:49 -0300

    ggsggsgs

---
+++ Ex010/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Ex010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, aumento;
+
+            Console.WriteLine("Cálculo de Salário.");
+
+            Console.WriteLine("Digite aqui o seu salario: ");
+            salario = Convert.ToInt32(Console.ReadLine());
+
+            if (salario > 2500)
+            {
+
+                aumento = (salario * 0.05);
+                Console.WriteLine("Seu aumento é de: {0}", aumento);
+                aumento = salario + (salario * 0.05);
+                Console.WriteLine("seu salário é de: {0}", aumento);
+
+            }
+            else if (salario > 1250)
+            {
+                aumento = (salario * 0.10);
+                Console.WriteLine("Seu aumento é de: {0}", aumento);
+                aumento = salario + (salario * 0.10);
+                Console.WriteLine("Seu salário é de: {0}", aumento);
+
+            }
+            else if(salario <= 1250)
+            {
+                aumento = (salario * 0.15);
+                Console.WriteLine("Seu aumento é de: {0}", aumento);
+                aumento = salario + (salario * 0.15);
+                Console.WriteLine("Seu salário final é de: {0}", aumento);
+
+            }
+            
+            
+
+            
+
+            
+
+
+        }
+    }
+}
