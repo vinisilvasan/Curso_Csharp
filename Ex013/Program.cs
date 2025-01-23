e60d3913f15958c33915b6f3f2fc727bece9bba6
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-23 20:46:20 -0300

    melhorias no for 2

---
+++ Ex013/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma, quant;
+
+            soma = quant = 0;
+
+            for (int i = 0; i <= 500; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    soma += i;
+                    quant++;
+                }
+
+            }
+            Console.Write("A soma dos {0} valores informados é {1}.", quant, soma);
+        }
+    }
+}
