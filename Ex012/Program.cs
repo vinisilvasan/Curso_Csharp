ee9237c4d2bcad1bbb6d1313f0ecc239a7bec480
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-23 20:03:39 -0300

    melhorias no for

---
+++ Ex012/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int final;
+            Console.Write("Digite um número: ");
+            final = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= final; i++)
+                
+                if (i % 2 == 0)
+                {
+                    Console.Write("{0}, ",i);
+                }
+            Console.WriteLine("O exercicio deu certo!!!");
+
+        }
+    }
+}
