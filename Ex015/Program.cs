24334e2fe4d38da3ba538e40142b74b5f4f0eca4
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-23 21:35:48 -0300

    while 1

---
+++ Ex015/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ex015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string genero;
+
+            Console.Write("Digite o gênero: ");
+            genero = Console.ReadLine().ToUpper();
+            
+
+            while(genero != "F" && genero != "M")
+            {
+                Console.Write("Digite o gênero: ");
+                genero = Console.ReadLine();
+
+
+            }
+
+            Console.WriteLine("Bem-vindos e bem-vindas a curso de C#!");
+
+
+        }
+    }
+}
