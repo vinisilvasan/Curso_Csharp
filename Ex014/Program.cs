e5a367cbf4a71515a070532f385295c1f8768005
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-23 21:20:30 -0300

    for 3

---
+++ Ex014/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            Console.Write("Digite um número:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+
+            {
+                Console.WriteLine(" {0} x {1} = {2}",num,i,num*i);
+            }
+        }
+    }
+}
