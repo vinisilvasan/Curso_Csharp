4927dabf9493fbd5ee9c9cd44440273267cc8a46
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-30 20:54:39 -0300

    metodos no C#sharp

---
+++ Metodos/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saudacao("Vinicius");
+            Soma(45555, 78784); 
+        }
+
+        static void Saudacao(string nome)
+        {
+            Console.WriteLine($"Olá {nome}, seja bem vindo!");
+
+        }
+        static void Soma(int a, int b)
+        {
+            int resultado = a + b;
+            Console.WriteLine($"A soma de {a} + {b} é igual a {resultado}");
+            Console.WriteLine("A soma de {0} + {1} ´igual a {2}.", a, b, resultado);
+            Console.WriteLine("A soma de " + a + " + " + b + " é igual a " + resultado);
+        }
+    }
+}
