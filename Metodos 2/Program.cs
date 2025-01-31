ca4b81ea6f50953dbb3d382f2a879147df7f6c7b
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-30 21:33:55 -0300

    hfhfhf

---
+++ Metodos 2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Metodos_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=====================");
+            Console.WriteLine("Análise de Triangulos");
+            Console.WriteLine("=====================");
+
+            Console.WriteLine("Digite o valor da base do triangulo: ");
+            double ba = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a altura do triangulo:");
+            double al = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            BaseAltura(ba, al);
+
+        }
+        static void BaseAltura(double a, double b)
+        {
+            double x = (b * a) / 2;
+            Console.WriteLine(x);
+
+        }
+    }
+}
