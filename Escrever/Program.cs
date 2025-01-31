45d0cc46d7e9770c38f9a61b9340cc2873be5c9e
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-30 21:49:46 -0300

    escrever

---
+++ Escrever/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Escrever
+{
+    internal class Program
+    {
+        static void Escrever(string texto)
+        {
+            int tam = texto.Length + 4;
+            string linha = new('*', tam);
+            string textocentralizado = texto.PadLeft(tam - 2).PadRight(tam - 2);
+            Console.WriteLine(linha);
+            Console.WriteLine(textocentralizado);
+            Console.WriteLine(linha);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Escrever("Vinicius Santana Silva");
+            Console.WriteLine();
+            Escrever("Curso C#");
+            Console.WriteLine();
+            Escrever("Olá");
+            Console.WriteLine();
+
+        }
+    }
+}
