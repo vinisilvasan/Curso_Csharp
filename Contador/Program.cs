2177f1da3e58e72bab9f331eeecdf049ac064cd4
Vinicius <vinicius.ssilva113@senacsp.edu.br>
2025-01-30 22:18:25 -0300

    escrever

---
+++ Contador/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading;
+
+namespace Contador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contador(1, 10, 1);
+            Contador(10, 0, 2);
+
+            Escrever("Agora é a sua vez de personaizar a contagem!");
+            Console.Write("Digite o início: ");
+            int ini = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o início: ");
+            int final = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o início: ");
+            int pas = Convert.ToInt32(Console.ReadLine());
+
+            Contador(ini, final, pas);
+
+        }
+
+        static void Contador(int inicio, int fim, int passo)
+        {
+            Escrever($"Contagem de {inicio} até {fim} de {passo} em {passo}");
+            Thread.Sleep(2000);
+
+            if (inicio < fim)
+            {
+                int cont = inicio;
+                while (cont <= fim)
+                {
+                    Console.Write($"{cont} ");
+                    Thread.Sleep(500);
+                    cont += passo;
+
+                }
+                Console.WriteLine("Fim!");
+
+            }
+            else
+            {
+                int cont = inicio;
+                while (cont >= fim)
+                {
+                    Console.Write($"{cont} ");
+                    Thread.Sleep(500);
+                    cont -= passo;
+
+                }
+                Console.WriteLine("Fim!");
+            }
+
+        }
+
+        static void Escrever(string texto)
+        {
+            int tam = texto.Length + 4;
+            string linha = new('=', tam);
+            string textocentralizado = texto.PadLeft(tam - 2).PadRight(tam - 2);
+            Console.WriteLine(linha);
+            Console.WriteLine(textocentralizado);
+            Console.WriteLine(linha);
+            
+        }
+
+    }
+}
